bdce4409f4a3ffee3d86e312ca1b2639015c6e1c
Fin Mead <finleymeadie@hotmail.co.uk>
2023-04-17 21:38:06 +0100

    Display accelerometer, gyro and pressure values

--- app/src/main/java/com/example/bikerino_prototype/MainActivity.java
+++ app/src/main/java/com/example/bikerino_prototype/MainActivity.java
@@ -10,10 +10,16 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
-    private Sensor accelerometer;
+    private Sensor accelerometerSensor;
+    private Sensor gyroscopeSensor;
+    private Sensor pressureSensor;
+    private TextView accelerometerValuesTextView;
+    private TextView gyroscopeValuesTextView;
+    private TextView pressureValuesTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +29,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Initialize SensorManager
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        // Initialize accelerometer sensor
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Initialize sensors
+        // Check if accelerometer sensor is available
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometerSensor == null) {
+            // Handle case when accelerometer sensor is not available
+            Toast.makeText(this, "Accelerometer sensor not available", Toast.LENGTH_SHORT).show();
+        }
+
+        // Check if gyroscope sensor is available
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscopeSensor == null) {
+            // Handle case when gyroscope sensor is not available
+            Toast.makeText(this, "Gyroscope sensor not available", Toast.LENGTH_SHORT).show();
+        }
+
+        // Check if pressure sensor is available
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if (pressureSensor == null) {
+            // Handle case when pressure sensor is not available
+            Toast.makeText(this, "Pressure sensor not available", Toast.LENGTH_SHORT).show();
+        }
+
+        // Initialize TextViews
+        accelerometerValuesTextView = findViewById(R.id.accelerometerValuesTextView);
+        gyroscopeValuesTextView = findViewById(R.id.gyroscopeValuesTextView2);
+        pressureValuesTextView = findViewById(R.id.pressureValuesTextView3);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         // Register the SensorEventListener
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -43,19 +75,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        TextView accelerometerValuesTextView = findViewById(R.id.accelerometerValuesTextView);
-        // Handle accelerometer values here
-        float x = event.values[0]; // Acceleration along x-axis
-        float y = event.values[1]; // Acceleration along y-axis
-        float z = event.values[2]; // Acceleration along z-axis
-
-        // Log accelerometer values
-//        Log.d("Accelerometer", "X: " + x + " Y: " + y + " Z: " + z);
-        float[] arr = new float[] {x, y, z};
-        String accelerometerValuesString = "\nX: " + x + "\nY: " + y + "\nZ: " + z;
-
-        // Update the text of the TextView
-        accelerometerValuesTextView.setText("Accelerometer Values: " + accelerometerValuesString);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            // Handle accelerometer values here
+            float ax = event.values[0]; // Acceleration along x-axis
+            float ay = event.values[1]; // Acceleration along y-axis
+            float az = event.values[2]; // Acceleration along z-axis
+            String accelerometerValuesString = "\nX: " + ax + "\nY: " + ay + "\nZ: " + az;
+            accelerometerValuesTextView.setText("\nAccelerometer Values: " + accelerometerValuesString);
+        }
+        // Handle gyroscope values
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float gx = event.values[0]; // Rotation rate around x-axis
+            float gy = event.values[1]; // Rotation rate around y-axis
+            float gz = event.values[2]; // Rotation rate around z-axis
+            String gyroscopeValuesString = "\nX: " + gx + "\nY: " + gy + "\nZ: " + gz;
+            gyroscopeValuesTextView.setText("\nGyroscope Values: " + gyroscopeValuesString);
+        }
+        // Handle pressure sensor value
+        if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            float pressure = event.values[0]; // Atmospheric pressure
+            String pressureValueString = String.valueOf(pressure);
+            pressureValuesTextView.setText("\nPressure Value: \n" + pressureValueString);
+        }
     }
 
     @Override
